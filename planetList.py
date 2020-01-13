7ad84866deb85b52d8a1f14f2083d3aa117b1e2a
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-13 15:12:38 -0600

    finished append and extend, not finished with exercise

---
+++ planetList.py
@@ -0,0 +1,10 @@
+planet_list = ["Mercury", "Mars"]
+
+# append adds one item to the list
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
+
+# extend adds more than one item to the list
+planet_list.extend(["Uranus", "Neptune"])
+
+

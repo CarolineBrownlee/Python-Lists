4a918ddf3b5ee6174e3fe9a8d9326d2d4311f74b
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-14 09:43:32 -0600

    finished through 3rd problem on exercise

--- planetList.py
+++ planetList.py
@@ -1,10 +1,18 @@
 planet_list = ["Mercury", "Mars"]
 
+# 1. Use the extend() method to add another list of the last two planets in our solar system to the end of the list.
 # append adds one item to the list
 planet_list.append("Jupiter")
 planet_list.append("Saturn")
 
+# 2. Use the extend() method to add another list of the last two planets in our solar system to the end of the list.
 # extend adds more than one item to the list
 planet_list.extend(["Uranus", "Neptune"])
+
+# 3. Use insert() to add Earth, and Venus in the correct order.
+# insert an item at a given position
+print(planet_list)
+planet_list.insert(1, "Venus")
+planet_list.insert(2, "Earth")
 
 
